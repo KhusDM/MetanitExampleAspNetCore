7ff239c3174a3d197564800d2b0beb49252dbfca
KhusnetdinovDmitrii <d.xusnetd@yandex.ru>
2018-05-03 16:55:00 +0300

    commit

--- MetanitExample/MetanitExample/Startup.cs
+++ MetanitExample/MetanitExample/Startup.cs
@@ -11,6 +11,30 @@ namespace MetanitExample
 {
     public class Startup
     {
+        private static void Index(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Index");
+            });
+        }
+
+        private static void About(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("About");
+            });
+        }
+
+        private static void HandleId(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("id is equal to 5");
+            });
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -25,9 +49,45 @@ namespace MetanitExample
                 app.UseDeveloperExceptionPage();
             }
 
+            //
+            //int x = 2;
+            //app.Use(async (context, next) =>
+            //{
+            //    x = x * 2;      // 2 * 2 = 4
+            //    await next.Invoke();    // вызов app.Run
+            //    x = x * 2;      // 8 * 2 = 16
+            //    await context.Response.WriteAsync($"Result: {x}");
+            //});
+
+            //app.Run(async (context) =>
+            //{
+            //    x = x * 2;  //  4 * 2 = 8
+            //    await Task.FromResult(0);
+            //});
+            //
+
+            //
+            //app.Map("/home", home =>
+            //{
+            //    home.Map("/index", Index);
+            //    home.Map("/about", About);
+            //});
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Page Not Found");
+            //});
+            //
+
+            app.MapWhen(context =>
+            {
+                return context.Request.Query.ContainsKey("id") &&
+                        context.Request.Query["id"] == "5";
+            }, HandleId);
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Good bye, World...");
             });
         }
     }
